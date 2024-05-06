5cdd0a05f60e60fa42e419eadfc5a55702a31c7e
Ishrat Ali <157359246+Ishrat72@users.noreply.github.com>
2024-05-06 12:54:11 +0500

    Add files via upload

---
+++ React/Friends.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+export default function Friends(){
+    const [friend, setFriend] = useState(" ");
+    const [firends, setFriends] = useState([]);
+    const [friendType, setFriendType] = useState("Acha");
+
+    return (
+            
+    )
+
+
+
+}
